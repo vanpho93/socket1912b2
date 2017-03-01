2c4a2e54d9f3164cd908a11de5892269f74b6940
Admin <vanpho02@gmail.com>
2017-03-01 20:31:18 +0700

    d

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let server = require('http').Server(app);//1
 let io = require('socket.io')(server);//2
 server.listen(3000, () => console.log('Server started'));//3
 
+let arrUsername = [];
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
@@ -13,6 +15,13 @@ app.get('/', (req, res) => res.render('home'));
 io.on('connection', socket => {
   console.log('Co nguoi ket noi');
   socket.on('CLIENT_SIGN_UP', username => {
-    console.log('New user: ' + username);
+    if(arrUsername.indexOf(username) == -1) {
+      arrUsername.push(username);
+      //Emit cho tat ca user
+      io.emit('NEW_USER_CONNECTED', username);
+      socket.emit('SERVER_ACCEPT_USERNAME');
+    } else{
+      socket.emit('SERVER_REJECT_USERNAME');
+    }
   });
 });
