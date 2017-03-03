894223d7fb912fa6da7cb68707932be4cd617134
Admin <vanpho02@gmail.com>
2017-03-03 19:47:34 +0700

    when start browser

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let server = require('http').Server(app);//1
 let io = require('socket.io')(server);//2
 server.listen(3000, () => console.log('Server started'));//3
 
-let arrUsername = [];
+let arrUsername = require('./socketControllers/arrUsername.js');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -14,6 +14,9 @@ app.get('/', (req, res) => res.render('home'));
 
 io.on('connection', socket => {
   console.log('Co nguoi ket noi');
+
+  socket.emit('LIST_ONLINE_USER', arrUsername);
+
   socket.on('CLIENT_SIGN_UP', require('./socketControllers/signUp.js')(io, socket));
 
   socket.on('disconnect', () => {
